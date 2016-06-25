6d737cf3bde8c0fbc119e23f8d253a2002ad2615
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-06-25 13:46:02 +0200

    add scroll to nav and cleanup

--- client/src/app/components/ui/views/navigator-view/navigator-view.component.ts
+++ client/src/app/components/ui/views/navigator-view/navigator-view.component.ts
@@ -10,66 +10,70 @@ import { File } from "../../../../shared/models/File";
   selector: 'asm-navigator-view',
   templateUrl: 'navigator-view.component.html',
   styleUrls: ['navigator-view.component.css'],
-  directives:[TreeNodeWidgetComponent],
-  providers:[FileService]
+  directives: [TreeNodeWidgetComponent],
+  providers: [FileService]
 })
 export class NavigatorViewComponent implements OnInit {
-  private _treeNodes:TreeNodeWidget[]=[];
+  private _treeNodes:TreeNodeWidget[] = [];
   private _selectedIndex:number;
   private _selectedList:number[];
-  private _isMultipleSelectionOn:boolean=false;
+  private _isMultipleSelectionOn:boolean = false;
   //TODO should be provided by project service
-  private _projectRoot:string='/an/absolute/path/to/project-name';
+  private _projectRoot:string = '/an/absolute/path/to/project-name';
 
-  constructor(private _fileService:FileService) {}
+  constructor(private _fileService:FileService) {
+  }
 
   ngOnInit() {
     let files:File[] = this._fileService.getFiles(this._projectRoot);
-    for (let file of files){
-      this._treeNodes.push(new TreeNodeWidget(file));
+    for (let file of files) {
+      let node:TreeNodeWidget = new TreeNodeWidget(file);
+      node.numOfChildren = 0;
+      this._treeNodes.push(node);
     }
   }
 
-  onExpanded(expOrColEvent:ExpandCollapseEvent,inx:number){
-    let files:File[] =this._fileService.getFiles(this._projectRoot+'/'+'src');
+  onExpanded(expOrColEvent:ExpandCollapseEvent, inx:number) {
+    let files:File[] = this._fileService.getFiles(this._projectRoot + '/' + 'src');
 
     /* expand */
-    if(expOrColEvent.expandOrCollapse){
-      let indentLevel = expOrColEvent.treeNodeWidget.indentLevel+1;
-      for (let file of files){
-        let node = new TreeNodeWidget(file,indentLevel);
-        this._treeNodes.splice(++inx,0,node);
+    if (expOrColEvent.expandOrCollapse) {
+      let indentLevel = expOrColEvent.treeNodeWidget.indentLevel + 1;
+      for (let file of files) {
+        let node = new TreeNodeWidget(file, indentLevel);
+        this._treeNodes.splice(++inx, 0, node);
       }
-      expOrColEvent.treeNodeWidget.numOfChildren=files.length;
+      expOrColEvent.treeNodeWidget.numOfChildren = files.length;
     }
     /* collapse */
     else {
-      let i:number=expOrColEvent.treeNodeWidget.numOfChildren;
-      let counter:number=0;
-      while(i!=0){
+      let i:number = expOrColEvent.treeNodeWidget.numOfChildren;
+      let counter:number = 0;
+      while (i != 0) {
         counter++;
         i--;
-        i+=this._treeNodes[inx+counter].numOfChildren;
+        i += this._treeNodes[inx + counter].numOfChildren;
       }
 
-      this._treeNodes.splice(++inx,counter);
+      this._treeNodes.splice(++inx, counter);
     }
   }
 
-  onSelected(inx:number){
-    if (this._isMultipleSelectionOn){
+  onSelected(inx:number) {
+    if (this._isMultipleSelectionOn) {
 
     }
     else {
-      this._selectedIndex=inx;
+      this._selectedIndex = inx;
     }
   }
-  isSelected(inx){
-    if (this._isMultipleSelectionOn){
+
+  isSelected(inx) {
+    if (this._isMultipleSelectionOn) {
 
     }
     else {
-      this._selectedIndex=inx;
+      this._selectedIndex = inx;
     }
     // if (this._treeNodes[inx].isSelected){
     //   return true;
