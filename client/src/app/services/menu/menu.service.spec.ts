967312b5d603cbd73684719df6fd696abc683c6f
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-06-30 16:15:24 +0200

    add menu service

---
+++ client/src/app/services/menu/menu.service.spec.ts
@@ -0,0 +1,19 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import { MenuService } from './menu.service';
+
+describe('Menu Service', () => {
+  beforeEachProviders(() => [MenuService]);
+
+  it('should ...',
+      inject([MenuService], (service: MenuService) => {
+    expect(service).toBeTruthy();
+  }));
+  
+});
