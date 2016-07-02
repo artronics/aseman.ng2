a2cea350d3aa137bd1b77993fbab0037e59b77db
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-07-02 02:26:24 +0200

    add addProviders to fix injection problem with tests

--- client/src/app/services/menu/menubar.service.spec.ts
+++ client/src/app/services/menu/menubar.service.spec.ts
@@ -1,33 +1,19 @@
 /* tslint:disable:no-unused-variable */
 
-import {
-  beforeEach, beforeEachProviders,
-  describe, xdescribe,
-  expect, it, xit,
-  async, inject
-} from '@angular/core/testing';
-import { MenubarService } from './menubar.service';
+import { beforeEach, addProviders, describe, expect, it, inject } from "@angular/core/testing";
+import { MenubarService } from "./menubar.service";
 import { MenuList } from "../../shared/menu/MenuList";
 import { ActionMenuItem } from "../../shared/menu/ActionMenuItem";
+import { MenubarItemFactory } from "../../components/ide/MenubarItemFactory";
 
 describe('Menubar Service', () => {
   let menubarService:MenubarService;
-  beforeEachProviders(() => [MenubarService]);
 
   beforeEach(()=>{
-    menubarService=new MenubarService();
-
-    let foo:MenuList=new MenuList('foo','Foo');
-    foo.addMenuItem(new ActionMenuItem('new','New'));
-    foo.addMenuItem(new ActionMenuItem('open','Open'));
-    let bar:MenuList=new MenuList('bar','Bar');
-    bar.addMenuItem(new ActionMenuItem('find','Find'));
-    bar.addMenuItem(new ActionMenuItem('copy','Copy'));
-
-    let lists:MenuList[]=[];
-    lists.push(foo);lists.push(bar);
-    menubarService.menuLists=lists;
-  })
+    addProviders([MenubarService,MenubarItemFactory]);
+    
+    menubarService=new MenubarService(new MenubarItemFactoryMock);
+  });
 
   it('should ...',
       inject([MenubarService], (service: MenubarService) => {
@@ -40,3 +26,23 @@ describe('Menubar Service', () => {
   });
 
 });
+class MenubarItemFactoryMock extends MenubarItemFactory{
+  private lists:MenuList[]=[];
+
+  constructor(){
+    super();
+    let foo:MenuList=new MenuList('foo','Foo');
+    foo.addMenuItem(new ActionMenuItem('new','New'));
+    foo.addMenuItem(new ActionMenuItem('open','Open'));
+    let bar:MenuList=new MenuList('bar','Bar');
+    bar.addMenuItem(new ActionMenuItem('find','Find'));
+    bar.addMenuItem(new ActionMenuItem('copy','Copy'));
+
+    this.lists.push(foo);
+    this.lists.push(bar);
+  }
+
+  get menubarMenuLists():MenuList[] {
+    return this.lists;
+  }
+}
