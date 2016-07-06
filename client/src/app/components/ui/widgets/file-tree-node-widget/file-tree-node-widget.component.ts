693161123445a6f8c5e0ef299458df08f44bb7c1
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-07-06 11:57:29 +0200

    add expansion logic

--- client/src/app/components/ui/widgets/file-tree-node-widget/file-tree-node-widget.component.ts
+++ client/src/app/components/ui/widgets/file-tree-node-widget/file-tree-node-widget.component.ts
@@ -1,32 +1,50 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { NgClass, NgStyle } from "@angular/common";
+import { File, FileType } from "../../../../shared/models/File";
 
 @Component({
   moduleId: module.id,
   selector: 'asm-file-tree-node-widget',
   template: `
-<span class="asm_widget asm" [ngStyle]="{'padding-left':padding}">
+<span class="asm_widget asm" [ngStyle]="{'padding-left':getPadding()}">
   <i (click)="toggleExpand($event)" [ngClass]="expandableIcon"></i>
   <span><i [ngClass]="nodeIcon"></i>
-  nodeItem</span><span>{{indentLevel}}</span>
+  {{fileNode.file.name}}</span><span>{{indentLevel}}</span>
 </span>`,
   styleUrls: ['file-tree-node-widget.component.css'],
   directives: [NgClass, NgStyle]
 })
 export class FileTreeNodeWidgetComponent implements OnInit {
   @Input()
-  indentLevel:number;
+  fileNode:FileNode;
+  @Output()
+  expanded:EventEmitter<any>=new EventEmitter();
+  @Output()
+  collapsed:EventEmitter<any>=new EventEmitter();
 
+  private _isExpandable:boolean=false;
+  private _isExpanded:boolean=false;
   private _expandableIcon:string;
   private _nodeIcon:string;
 
   constructor() {}
 
   ngOnInit() {
+    switch (this.fileNode.file.fileType){
+      case FileType.FILE:
+        this._isExpandable=false;
+        this._nodeIcon='fa fa-fw fa-file-o';
+        break;
+      case FileType.DIR:
+        this._isExpandable=true;
+        this._nodeIcon='fa fa-fw fa-folder';
+        break;
+    }
   }
 
   toggleExpand(event){
-
+    this._isExpanded=!this._isExpanded;
+    this._isExpanded?this.expanded.emit(null):this.collapsed.emit(null);
   }
 
   get nodeIcon():string {
@@ -34,6 +52,32 @@ export class FileTreeNodeWidgetComponent implements OnInit {
   }
 
   get expandableIcon():string {
-    return this._expandableIcon;
+    let icon:string='fa fa-fw ';
+    if(this._isExpandable){
+      icon+=this._isExpanded?'fa-caret-down':'fa-caret-right';
+    }
+    return icon;
+  }
+
+  getPadding(){
+    return String(this.fileNode.indentLevel)+'em';
+  }
+
+}
+export class FileNode{
+  private _file:File;
+  private _indentLevel:number;
+
+  constructor(file:File, indentLevel:number) {
+    this._file = file;
+    this._indentLevel = indentLevel;
+  }
+
+  get file():File {
+    return this._file;
+  }
+
+  get indentLevel():number {
+    return this._indentLevel;
   }
 }
--- client/src/app/components/ui/widgets/file-tree-widget/file-tree-widget.component.ts
+++ client/src/app/components/ui/widgets/file-tree-widget/file-tree-widget.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { FileService } from "../../../../services/FileService";
 import { File } from "../../../../shared/models/File";
-import { FileTreeNodeWidgetComponent } from "../file-tree-node-widget/file-tree-node-widget.component";
+import { FileTreeNodeWidgetComponent, FileNode } from "../file-tree-node-widget/file-tree-node-widget.component";
 
 @Component({
   moduleId: module.id,
   selector: 'asm-file-tree-widget',
   template:`
 <ul>
-  <li *ngFor="let file of files; let inx=index" [class.tree-item-selected]="isSelected(inx)"
-      (click)="onSelected(inx)">{{file.name}}
-    <asm-file-tree-node-widget [node]="node" [indentLevel]="indentLevel(inx)"
+  <li *ngFor="let node of fileNodes; let inx=index" [class.tree-item-selected]="isSelected(inx)"
+      (click)="onSelected(inx)">
+    <asm-file-tree-node-widget [fileNode]="node" [indentLevel]="indentLevel(inx)"
                        [isSelected]="isSelected(inx)"
-                       (onExpanded)="onExpansion($event,inx)">
+                       (expanded)="onExpansion($event,inx)" (collapsed)="onCollapse(inx)">
     </asm-file-tree-node-widget>
   </li>
 </ul>
@@ -23,7 +23,7 @@ import { FileTreeNodeWidgetComponent } from "../file-tree-node-widget/file-tree-
 })
 export class FileTreeWidgetComponent implements OnInit {
   private _fileService:FileService;
-  private _files:File[]=[];
+  private _fileNodes:FileNode[]=[];
   private _projectPath:string='/an/absolute/path/to/project-name';
 
   constructor(fileService:FileService) {
@@ -31,14 +31,25 @@ export class FileTreeWidgetComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._files=this._fileService.getFiles(this._projectPath);
+    let files:File[]=this._fileService.getFiles(this._projectPath);
+    for(let f of files){
+      this._fileNodes.push(new FileNode(f,0));
+    }
   }
 
   indentLevel(inx:number){
 
   }
   onExpansion(event,inx:number){
-
+    let file:File=this._fileNodes[inx].file;
+    let newIndentLevel:number=this._fileNodes[inx].indentLevel+1;
+    let files:File[]=this._fileService.getFiles(this._projectPath+'/src');
+    for(let f of files){
+      this._fileNodes.splice(++inx,0,new FileNode(f,newIndentLevel));
+    }
+  }
+  onCollapse(inx:number){
+    console.log('col');
   }
 
   onSelected(inx:number){
@@ -46,10 +57,10 @@ export class FileTreeWidgetComponent implements OnInit {
   }
 
   isSelected(inx:number){
-    
+
   }
 
-  get files():File[] {
-    return this._files;
+  get fileNodes():FileNode[] {
+    return this._fileNodes;
   }
 }
